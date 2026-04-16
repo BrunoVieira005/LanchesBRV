bd6d7fe6d3b38ceca287e078c8b661366d5b085e
BrunoVieira005 <brunorv2005@gmail.com>
2026-04-16 19:51:02 -0300

    feat: criação dos modelos Lanche e Categoria com definição do relacionamento, e alteração da config do projeto nullable

---
+++ LanchesBRV/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace LanchesBRV.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        // Define o relacionamento com a categoria Lanche de 1 para muitos
+        public List<Lanche> Lanches { get; set; }
+    }
+}
---
+++ LanchesBRV/Models/Lanche.cs
@@ -0,0 +1,20 @@
+﻿namespace LanchesBRV.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }   
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        // Propriedades de navegação para definição do relacionamento
+        public int CategoriaId { get; set; } // Foreign Key
+        public virtual Categoria Categoria { get; set; }
+
+    }
+}
