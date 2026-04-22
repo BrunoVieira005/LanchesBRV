56372d2ebaee2fbbc6457df0738e71e98a8837d7
BrunoVieira005 <brunorv2005@gmail.com>
2026-04-22 14:00:05 -0300

    feat: criação de controller e view para exibição dos lanches

---
+++ LanchesBRV/Controllers/LancheController.cs
@@ -0,0 +1,27 @@
+﻿using LanchesBRV.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesBRV.Controllers
+{
+    // Controller responsável por gerenciar as ações (requests) relacionadas aos lanches
+    public class LancheController : Controller
+    {
+        // Armazena a instância do repositório de maneira protegida e imutável (readonly)
+        private readonly ILancheRepository _lancheRepository;
+
+        // Define o construtor que recebe a instância do Repository para Lanche
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        // Action que será chamada pela rota (/Lanche/List)
+        public IActionResult List()
+        {
+            // Obtém os dados através do repository, com todos os lanches
+            var lanches = _lancheRepository.Lanches;
+            // Retorna a View padrão (List.cshtml) passando a lista de lanches como Model
+            return View(lanches);
+        }
+    }
+}
