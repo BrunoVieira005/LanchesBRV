afc67ea152263a3df07e9c1276e953bb267343da
BrunoVieira005 <brunorv2005@gmail.com>
2026-04-30 16:10:00 -0300

    feat: rota personalizada pra categoria, ViewComponent para exibição de categorias, botão para redirecionar ao carrinho

--- LanchesBRV/Controllers/LancheController.cs
+++ LanchesBRV/Controllers/LancheController.cs
@@ -1,7 +1,9 @@
-﻿using LanchesBRV.Repositories;
+﻿using LanchesBRV.Models;
+using LanchesBRV.Repositories;
 using LanchesBRV.Repositories.Interfaces;
 using LanchesBRV.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace LanchesBRV.Controllers
 {
@@ -18,16 +20,53 @@ namespace LanchesBRV.Controllers
         }
 
         // Action que será chamada pela rota (/Lanche/List)
-        public IActionResult List()
-        {
-            // Instancia a ViewModel que servirá os dados para a View
-            var lanchesListViewModel = new LancheListViewModel();
-            // Preenche a propriedade Lanches da ViewModel com os dados vindos do repositório
-            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+        /* public IActionResult List()
+         {
+             // Instancia a ViewModel que servirá os dados para a View
+             // var lanchesListViewModel = new LancheListViewModel();
+             // Preenche a propriedade Lanches da ViewModel com os dados vindos do repositório
+             //lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+             lanchesListViewModel.CategoriaAtual = "Categoria Atual";
 
-            // Retorna a View com as propriedades definidas na ViewModel
+             // Retorna a View com as propriedades definidas na ViewModel
+             return View(lanchesListViewModel);
+         }*/
+        public IActionResult List(string categoria)
+        {
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+
+
+            if (string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                categoriaAtual = "Todos os lanches";
+            }
+
+            else
+            {
+                lanches = _lancheRepository.Lanches
+                            .Where(l => l.Categoria.CategoriaNome.Equals(categoria))
+                            .OrderBy(c => c.Nome);
+
+                categoriaAtual = categoria;
+            }
+
+            var lanchesListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            };
+
             return View(lanchesListViewModel);
+        }
+
+        public IActionResult Details(int lancheId)
+        {
+
+            var lanche = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+            return View(lanche);
+
         }
     }
 }
